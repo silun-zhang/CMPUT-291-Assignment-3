92b82231e9883b190eb52f4db9f1b0b9269732ba
Silun Zhang <silun.canada@gmail.com>
2021-11-15 17:48:36 -0700

    Add Main File
    - Add A3Main.py:
    Use to generate graphs using A3Small.db, A3Medium.db, A3Large.db, in three scenarios (Uninformed, Self-optimized, User-optimized)
    Uses Q1A3.py, Q2A3.py, Q3A3.py
    Generates 3 .png files (Q1A3chart.png, Q2A3chart.png, Q3A3chart.png)

---
+++ A3Main.py
@@ -0,0 +1,26 @@
+# A3Main.py
+import Q1A3.py
+import Q2A3.py
+import Q3A3.py
+
+# Generate graph 1
+def graph_one():
+    return
+
+# Generate graph 2
+def graph_two():
+    return
+
+# Generate graph 3
+def graph_three():
+    return
+
+# Generates layered bar chart
+def Layered_bar_chart():
+    return
+
+def main():
+    return
+
+if __name__ == "__main__":
+    return
